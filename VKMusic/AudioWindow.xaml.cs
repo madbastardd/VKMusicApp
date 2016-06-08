0549224a4a5b43c0dc144b5f46099ac7012dc451
madbastardd <bastardd666@gmail.com>
2016-06-08 21:52:56 +0300

    download music updated

--- VKMusic/AudioWindow.xaml.cs
+++ VKMusic/AudioWindow.xaml.cs
@@ -1,8 +1,9 @@
 ﻿using MahApps.Metro.Controls;
 using System;
-using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.IO;
 using System.Net;
+using System.Threading;
 using System.Windows.Controls;
 using System.Windows.Markup;
 using System.Xml;
@@ -89,9 +90,16 @@ namespace VKMusic {
             //get Button that was clicked
             var btn = sender as System.Windows.Controls.Button;
 
-            media.Source = btn.Tag as Uri;
+            if (sender == playGlobalSong) {
+                //if clicked playGlobalSong
+            }
+            else {
+                //get song from tag (URL)
+                media.Source = btn.Tag as Uri;
 
-            media.Play();
+                //play it
+                media.Play();
+            }
         }
 
         private void downloadSong_Click(object sender, System.Windows.RoutedEventArgs e) {
@@ -99,8 +107,42 @@ namespace VKMusic {
             var btn = sender as System.Windows.Controls.Button;
 
             //Download file
-            using (WebClient web = new WebClient()) {
-                web.DownloadFile((btn.Tag as Uri).AbsoluteUri, "1.mp3");
+            using (WebClient client = new WebClient()) {
+                //thread to download file async
+                Thread thread = new Thread(() => {
+                    //make visible progress bar
+                    downloadProgressGrid.Dispatcher.Invoke(() => {
+                        downloadProgressGrid.Visibility = System.Windows.Visibility.Visible;
+                    });
+
+                    //method to change download progress
+                    client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(
+                        (object objSender, DownloadProgressChangedEventArgs ev) => {
+                            //get downloaded bytes
+                            double bytesIn = double.Parse(ev.BytesReceived.ToString());
+                            //get total bytes
+                            double totalBytes = double.Parse(ev.TotalBytesToReceive.ToString());
+                            //get part that was downloaded
+                            double percentage = bytesIn / totalBytes * 100;
+
+                            downloadProgress.Dispatcher.Invoke(() => downloadProgress.Value = percentage);
+                        });
+
+                    //method would call when file would be downloaded
+                    client.DownloadFileCompleted += new AsyncCompletedEventHandler(
+                        (object objSender, AsyncCompletedEventArgs ev) => {
+                            //hide progress bar
+                            downloadProgressGrid.Dispatcher.Invoke(() => {
+                                downloadProgressGrid.Visibility = System.Windows.Visibility.Hidden;
+                            });
+                    });
+
+                    //start download async
+                    btn.Dispatcher.Invoke(() => client.DownloadFileAsync(btn.Tag as Uri, "1.mp3"));
+                });
+
+                //start download
+                thread.Start();
             }
             
         }
